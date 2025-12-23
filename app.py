3e642801897eaa524dd4f929f3f7e5b3b790f9b8
Kostya <beztalannyjkonstantin@gmail.com>
2025-12-23 14:12:10 +0000

    a

--- app.py
+++ app.py
@@ -5,22 +5,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def main() -> str:
-    return "Home page"
+    return "Welcome to the app"
 
 
-@app.route("/profile/<username>")
-def profile(username: str) -> str:
-    return f"Profile of {username}"
+@app.route("/hello/<username>")
+def hello(username: str) -> str:
+    return f"Hello, {username}"
 
 
-@app.route("/access/<int:age>")
-def access(age: int) -> str:
+@app.route("/age/<int:age>")
+def age(age: int) -> str:
     return "Access granted" if age >= 18 else "Access denied"
 
 
 @app.route("/product/<name>/<int:price>")
 def product(name: str, price: int) -> str:
-    return f"Product {name} is {"expensive" if price > 1000 else "affordable"}"
+    if price < 20:
+        return f"Product {name} is cheap"
+    elif price > 100:
+        return f"Product {name} is expensive"
+    else:
+        return f"Product {name} is affortable"
+
+
+@app.route("/user/<username>/<int:posts>")
+def user(username: str, posts: int) -> str:
+    return f"{username is {'active' if posts >= 5 else 'inactive'}}"
 
 
 if __name__ == "__main__":
