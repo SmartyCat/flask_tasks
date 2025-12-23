d17b29074a3fb0e35311a5e1748071178faca563
Kostya <beztalannyjkonstantin@gmail.com>
2025-12-23 11:50:17 +0000

    firs

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 @app.route("/")
-def home():
+def main() -> str:
     return "Hello from Flask"
+
+
+@app.route("/about")
+def about() -> str:
+    return "About page"
