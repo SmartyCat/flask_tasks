0622d6c24812fc3c1e96c09937e14f79ecb9d6af
Kostya <beztalannyjkonstantin@gmail.com>
2025-12-23 13:46:45 +0000

    a

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ def about() -> str:
     return "About page"
 
 
-@app.route(f"/user/<username>")
-def profile(username: str) -> str:
-    return f"hello,{username}"
+@app.route("/user/<name>/<int:age>")
+def user(name: str, age: int) -> str:
+    return f"User {name}, age {age}"
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     app.run(debug=True)
