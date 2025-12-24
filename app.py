f998c03a609fe4e1c2b20d4eb0e53eba53134498
Kostya <beztalannyjkonstantin@gmail.com>
2025-12-24 09:26:51 +0000

    a

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+users = [
+    {"name": "Kostya", "vip": True},
+    {"name": "Anna", "vip": False},
+    {"name": "Sergey", "vip": True},
+]
+
+
 @app.route("/")
 def index() -> str:
-    return render_template("index.html", username="Kostya", is_auth=False)
+    return render_template("index.html")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/users")
+def users_func() -> str:
+    return render_template("users.html", users=users)
