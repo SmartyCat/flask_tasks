e5912147bc9bc5e11bea328a3b19a7065815c0e0
Kostya <beztalannyjkonstantin@gmail.com>
2025-12-24 11:08:45 +0000

    a

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 from flask import Flask, render_template
 
+users = [
+    {"name": "Kostya", "vip": True, "age": 30, "active": True},
+    {"name": "Anna", "vip": False, "age": 16, "active": False},
+    {"name": "Sergey", "vip": True, "age": 22, "active": False},
+]
+
+products = [
+    {"name": "Hammer", "category": "tools", "price": 44, "in_stock": True},
+    {"name": "Tomato", "category": "food", "price": 22, "in_stock": False},
+    {"name": "Vacuum", "category": "electronics", "price": 5, "in_stock": True},
+]
+
 app = Flask(__name__)
 
 
-users = [
-    {"name": "Kostya", "vip": True},
-    {"name": "Anna", "vip": False},
-    {"name": "Sergey", "vip": True},
-]
-
-
 @app.route("/")
-def index() -> str:
+def main() -> str:
     return render_template("index.html")
 
 
 @app.route("/users")
-def users_func() -> str:
+def users_function() -> str:
     return render_template("users.html", users=users)
+
+
+@app.route("/products")
+def products_function() -> str:
+    return render_template("products.html", products=products)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
