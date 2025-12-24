b1d6390658a93e49b9eeef75626cac0d4c5bcff4
Kostya <beztalannyjkonstantin@gmail.com>
2025-12-24 09:04:08 +0000

    a

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index() -> str:
-    return render_template("templates/index.html", title="Home", message="Hello from Flask")
+    return render_template("index.html", username="Kostya", is_auth=False)
 
 
 if __name__ == "__main__":
