6d12aa969daa341a01ac32b340c29a52e20d8b45
Kostya <beztalannyjkonstantin@gmail.com>
2025-12-24 08:50:32 +0000

    first

--- app.py
+++ app.py
@@ -1,36 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def main() -> str:
-    return "Welcome to the app"
-
-
-@app.route("/hello/<username>")
-def hello(username: str) -> str:
-    return f"Hello, {username}"
-
-
-@app.route("/age/<int:age>")
-def age(age: int) -> str:
-    return "Access granted" if age >= 18 else "Access denied"
-
-
-@app.route("/product/<name>/<int:price>")
-def product(name: str, price: int) -> str:
-    if price < 20:
-        return f"Product {name} is cheap"
-    elif price > 100:
-        return f"Product {name} is expensive"
-    else:
-        return f"Product {name} is affortable"
-
-
-@app.route("/user/<username>/<int:posts>")
-def user(username: str, posts: int) -> str:
-    return f"{username is {'active' if posts >= 5 else 'inactive'}}"
+def index() -> str:
+    return render_template("templates/index.html", title="Home", message="Hello from Flask")
 
 
 if __name__ == "__main__":
