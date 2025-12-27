b4b6277ae794836de11e93acee57ec135ad763e5
Kostya <beztalannyjkonstantin@gmail.com>
2025-12-27 09:28:12 +0000

    a

--- app.py
+++ app.py
@@ -1,35 +1,27 @@
-from flask import Flask, render_template
-
-users = [
-    {"id": 1, "name": "Kostya", "vip": True, "age": 30, "active": True},
-    {"id": 2, "name": "Anna", "vip": False, "age": 16, "active": False},
-    {"id": 3, "name": "Sergey", "vip": True, "age": 22, "active": False},
-    {"id": 4, "name": "Ivan", "vip": False, "age": 19, "active": True},
-]
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+users = [
+    {"id": 1, "name": "Kostya"},
+    {"id": 2, "name": "Anna"},
+    {"id": 3, "name": "Sergey"},
+]
+
 
 @app.route("/")
-def index() -> str:
+def main() -> str:
     return render_template("index.html")
 
 
 @app.route("/users")
 def users_function() -> str:
+    return render_template("users.html", users=users)
+
+
+@app.route("/user/<int:id>")
+def user_function(id: int) -> str:
     for user in users:
-        result = []
-        if user["vip"]:
-            result.append("VIP")
-        if user["active"]:
-            result.append("ACTIVE")
-        if not user["active"]:
-            result.append("INACTIVE")
-        if user["age"] < 18:
-            result.append("UNDERAGE")
-        user["status"] = " ".join(result)
-    return render_template("/users.html", users=users)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+        if user["id"] == id:
+            return render_template("/user.html", user=user["name"])
+    return render_template("user.html")
