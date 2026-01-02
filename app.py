68c5fd89808bcb6618f6d14840638f7ac9a860c7
Kostya <beztalannyjkonstantin@gmail.com>
2026-01-02 11:12:18 +0000

    a

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-from flask import Flask, render_template
-
-users = [
-    {"id": 1, "name": "Kostya", "vip": True, "active": True},
-    {"id": 2, "name": "Anna", "vip": False, "active": False},
-    {"id": 3, "name": "Sergey", "vip": True, "active": False},
-    {"id": 4, "name": "Ivan", "vip": False, "active": True},
-]
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -16,18 +8,6 @@ def home() -> str:
     return render_template("home.html")
 
 
-@app.route("/users")
-def users_func() -> str:
-    return render_template("users.html", users=users)
-
-
-@app.route("/users/active")
-def users_active_func() -> str:
-    return render_template(
-        "users.html", users=[user for user in users if user["active"]]
-    )
-
-
-@app.route("/users/vip")
-def users_vip_func() -> str:
-    return render_template("users.html", users=[user for user in users if user["vip"]])
+@app.route("/hello", methods=["POST"])
+def hello() -> str:
+    return render_template("hello.html", name=request.form["name"])
