90d234cabde6e8e4c1937efb9bff0b5a016b2a18
Kostya <beztalannyjkonstantin@gmail.com>
2026-01-02 13:11:43 +0000

    a

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
 
 
@@ -8,6 +9,8 @@ def home() -> str:
     return render_template("home.html")
 
 
-@app.route("/hello", methods=["POST"])
-def hello() -> str:
-    return render_template("hello.html", name=request.form["name"])
+@app.route("/result", methods=["POST"])
+def result() -> str:
+    return render_template(
+        "result.html", name=request.form.get("name"), age=int(request.form.get("age"))
+    )
