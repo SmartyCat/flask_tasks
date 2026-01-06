083e3b68045e0230d634986d6acdba7beede7542
Kostya <beztalannyjkonstantin@gmail.com>
2026-01-06 13:09:10 +0000

    a

--- app.py
+++ app.py
@@ -1,29 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, flash, request
 
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "dbdgb"
 
-@app.route("/")
+
+@app.route("/",methods=["GET","POST"])
 def home() -> str:
+    if request.method == "POST":
+        name = request.form.get("name")
+        if name:
+            flash (f"Hello, {name}!")
+        else:
+            flash ("Name is reqired!")
     return render_template("home.html")
-
-
-@app.route("/profile")
-def profile() -> str:
-    return render_template("profile.html")
-
-
-@app.route("/summary", methods=["POST"])
-def summary() -> str:
-    return render_template(
-        "summary.html",
-        age=int(request.form.get("age")),
-        name=request.form.get("name"),
-        city=request.form.get("city"),
-    )
-
-
-@app.route("/about")
-def about() -> str:
-    return render_template("about.html")
