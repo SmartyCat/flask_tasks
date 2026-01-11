5355ee77b48ac4e84f37d42733b02f29a70dbcec
Kostya <beztalannyjkonstantin@gmail.com>
2026-01-11 09:53:23 +0000

    a

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
-from flask import Flask, render_template, request, redirect, session, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash, session
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "sdfjbkfghbdlf"
+app.config["SECRET_KEY"] = "dfkdk"
 
 
 @app.route("/", methods=["GET", "POST"])
 def home() -> str:
-    if "name_age" in session:
+    if "person" in session:
         return redirect(url_for("profile"))
-
     elif request.method == "POST":
-        name = request.form.get("name")
-        age = request.form.get("age")
-        session["name_age"] = (name, age)
-        return redirect(url_for("profile", name=name, age=age))
+
+        name, age, sex, hobbie = (
+            request.form.get("name"),
+            int(request.form.get("age")),
+            request.form.get("sex"),
+            request.form.getlist("hobbie"),
+        )
+        if not name:
+            flash("Name is required", category="error")
+        elif age < 13:
+            flash("You are too young", category="error")
+        if name and age > 13 and sex and hobbie:
+            session["person"] = (name, age, sex, hobbie)
+            return redirect(url_for("profile"))
+        else:
+            flash("Please fill the form first", category="error")
     return render_template("home.html")
 
 
 @app.route("/profile")
 def profile() -> str:
-    return render_template(
-        "profile.html", name=session["name_age"][0], age=int(session["name_age"][1])
-    )
+    return render_template("profile.html")
 
 
-@app.route("/clear")
-def cleaar() -> str:
+@app.route("/logout")
+def logout() -> str:
     session.clear()
     return redirect(url_for("home"))
