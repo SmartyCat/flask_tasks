677b0ef033465ed4cd6c9f023c6382465417ddc2
Kostya <beztalannyjkonstantin@gmail.com>
2026-01-11 07:47:29 +0000

    a

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "sdfvsfv"
+names = []
+app.config["SECRET_KEY"] = "dfkbjndjgfo"
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/", methods=["GET", "POST"])
 def home() -> str:
     name = request.form.get("name")
+    if name:
+        names.append(name)
     if request.method == "POST":
         if name:
-            flash(f"Hello, {name}!", category="success")
+            flash("Success", category="success")
         else:
-            flash("Name is required",category="error")
-    return render_template("home.html")
+            flash("Error", category="error")
+    return render_template("home.html", name=name)
+
+
+@app.route("/profile")
+def profile() -> str:
+    return render_template("profile.html", name=names[-1])
+
+
+@app.errorhandler(404)
+def error(error) -> str:
+    return render_template("error.html")
