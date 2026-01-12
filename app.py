0363af758f5ccabc04ad321c3c68245140daa1fe
Kostya <beztalannyjkonstantin@gmail.com>
2026-01-12 09:44:03 +0000

    a

--- app.py
+++ app.py
@@ -1,39 +1,54 @@
-from flask import Flask, render_template, redirect, request, url_for, flash, session
+from flask import Flask, request, redirect, render_template, flash, session, url_for
+
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "dfkdk"
+app.config["SECRET_KEY"] = "gfbrgkj"
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home() -> str:
     if "person" in session:
-        return redirect(url_for("profile"))
-    elif request.method == "POST":
-
-        name, age, sex, hobbie = (
-            request.form.get("name"),
-            int(request.form.get("age")),
-            request.form.get("sex"),
-            request.form.getlist("hobbie"),
-        )
-        if not name:
-            flash("Name is required", category="error")
-        elif age < 13:
-            flash("You are too young", category="error")
-        if name and age > 13 and sex and hobbie:
-            session["person"] = (name, age, sex, hobbie)
-            return redirect(url_for("profile"))
-        else:
-            flash("Please fill the form first", category="error")
+        return redirect(url_for("dashboard"))
     return render_template("home.html")
 
 
-@app.route("/profile")
-def profile() -> str:
-    return render_template("profile.html")
+@app.route("/form", methods=["GET", "POST"])
+def form() -> str:
+    if request.method == "POST":
+        name, city = request.form.get("name"), request.form.get("city")
+        if name and city and not session:
+            session["person"] = (name, city)
+            flash("Profile created", category="success")
+            return redirect(url_for("dashboard"))
+        elif name and city and session:
+            session["person"] = (name, city)
+            flash("Profile update", category="success")
+            return redirect(url_for("dashboard"))
+        else:
+            flash("Fill all fields", category="error")
+    return render_template("form.html")
+
+
+@app.route("/dashboard")
+def dashboard() -> str:
+    if not session:
+        flash("Access denied", category="error")
+        return redirect(url_for("form"))
+    return render_template("dashboard.html")
+
+
+@app.route("/edit")
+def edit() -> str:
+    return redirect(url_for("form"))
 
 
 @app.route("/logout")
 def logout() -> str:
     session.clear()
+    flash("Logged out", category="success")
     return redirect(url_for("home"))
+
+
+@app.errorhandler(404)
+def error(error) -> str:
+    return render_template("error.html")
