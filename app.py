9070ec70239a6928e805cab2a644e7fcb64ce569
Kostya <beztalannyjkonstantin@gmail.com>
2026-01-15 13:03:04 +0000

    a

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, redirect, g, request, url_for
 import sqlite3
 
-
 DATABASE = "test.db"
-SECRET_KEY = "sfdf"
 DEBUG = True
+SECRET_KEY = "sfvsf"
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 
-def connect_db():
-    conn = sqlite3.connect(app.config["DATABASE"])
-    return conn
+def connect_db() -> sqlite3.Connection:
+    return sqlite3.connect(app.config["DATABASE"])
 
 
 def create_db() -> None:
@@ -23,9 +21,10 @@ def create_db() -> None:
     db.close()
 
 
-def get_db() -> None:
+def get_db() -> sqlite3.Connection:
     if not hasattr(g, "link_db"):
         g.link_db = connect_db()
+    return g.link_db
 
 
 @app.teardown_appcontext
@@ -36,5 +35,19 @@ def close_db(error) -> None:
 
 @app.route("/")
 def index() -> str:
-    db = connect_db()
-    return render_template("index.html", users=db.cursor().fetchall())
+    db = get_db()
+    return render_template(
+        "index.html", users=db.cursor().execute("SELECT * FROM users").fetchall()
+    )
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add() -> str:
+    db = get_db()
+    if request.method == "POST":
+        name, age = request.form.get("name"), request.form.get("age")
+        db.cursor().execute("INSERT INTO users(name, age) VALUES(?, ?)", (name, age))
+        db.commit()
+        db.close()
+        return redirect(url_for("index"))
+    return render_template("add.html")
