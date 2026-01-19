e80637b24ff3d276e52ed6204b5a4c96a7711885
Kostya <beztalannyjkonstantin@gmail.com>
2026-01-19 09:04:46 +0000

    a

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, make_response
-
+from flask import Flask, make_response, request
 
 DEBUG = True
-SECRET_KEY = "dfbdgfb"
-
+SECRET_KEY = "sdfvdf"
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 
 @app.route("/")
-def hello() -> str:
-    resp = make_response("Hello, Flask!")
-    resp._status_code = 200
-    resp.headers["X-App-Name"] = "FlaskTraining"
+def index() -> str:
+    username = request.form.get("username")
+    if username:
+        result = "Hello, {username}"
+    else:
+        result = "Hello, stranger"
+    resp = make_response(result)
+    resp.set_cookie("username", "Alex")
 
     return resp
+
