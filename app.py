cee47b179273897999ad9be3326e9860eafbc7c9
Kostya <beztalannyjkonstantin@gmail.com>
2026-01-21 13:41:20 +0000

    a

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
-from flask import Flask, render_template, redirect, session, url_for, request
+from flask import Flask, render_template, request, session, url_for, redirect, flash
 
-SECRET_KEY = "sdkvj12e23sld"
-
+SECRET_KEY = "sdvjdfvl12w4d0sfvd34"
+DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index() -> str:
-    if "count" not in session:
-        session["count"] = 0
+    return redirect(url_for("login"))
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login() -> str:
     if request.method == "POST":
-        session["count"] += 1
-        return redirect(url_for("index"))
-    return render_template("index.html", count=session["count"])
+        username = request.form.get("username")
+        if not username:
+            flash("Error", category="error")
+        else:
+            session["username"] = username
+            return redirect(url_for("profile"))
+    return render_template("login.html")
+
+
+@app.route("/profile")
+def profile() -> str:
+    if "username" not in session:
+        return redirect(url_for("login"))
+    return render_template("profile.html", username=session["username"])
+
+
+@app.route("/logout")
+def logout() -> str:
+    session.clear()
+    return redirect(url_for("login"))
