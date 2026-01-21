a3fe12ee5c3b2a5faa9bf1dd48e811b721af114b
Kostya <beztalannyjkonstantin@gmail.com>
2026-01-21 14:22:53 +0000

    a

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, session, url_for, redirect, flash
+from flask import Flask, session, render_template, redirect, request, flash, url_for
 
-SECRET_KEY = "sdvjdfvl12w4d0sfvd34"
+
+SECRET_KEY = "SJFVHFDIU8234SDVK23"
 DEBUG = True
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 
@@ -11,26 +13,43 @@ def index() -> str:
     return redirect(url_for("login"))
 
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route("/login", methods=["GET", "POST"])
 def login() -> str:
     if request.method == "POST":
         username = request.form.get("username")
         if not username:
-            flash("Error", category="error")
+            flash("error", category="error")
         else:
             session["username"] = username
-            return redirect(url_for("profile"))
+            session["actions"] = 0
+            return redirect(url_for("dashboard"))
     return render_template("login.html")
 
 
-@app.route("/profile")
-def profile() -> str:
+@app.route("/dashboard")
+def dashboard() -> str:
     if "username" not in session:
         return redirect(url_for("login"))
-    return render_template("profile.html", username=session["username"])
+    return render_template(
+        "dashboard.html",
+        username=session.get("username"),
+        actions=session.get("actions"),
+    )
+
+
+@app.route("/action")
+def action() -> str:
+    if "username" not in session:
+        return redirect(url_for("login"))
+    session["actions"] += 1
+    flash("Action counted", category="success")
+    return redirect(url_for("dashboard"))
 
 
 @app.route("/logout")
 def logout() -> str:
+    if "username" not in session:
+        return redirect(url_for("login"))
     session.clear()
+    flash("Logged out", category="success")
     return redirect(url_for("login"))
