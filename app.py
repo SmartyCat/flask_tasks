d8a643761f92fbf4f9048dea9b16ef658f38049e
Kostya <beztalannyjkonstantin@gmail.com>
2026-01-23 12:24:17 +0000

    a

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import render_template, request, g, flash, Flask, redirect, url_for
+from flask import Flask, redirect, render_template, flash, request, session, url_for, g
 import sqlite3
+from flask_login import LoginManager, login_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from userlogin import UserLogin
 
 DATABASE = "users.db"
 DEBUG = True
-SECRET_KEY = "sdfng1l"
+SECRET_KEY = "sfkgm2;s;fv234"
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+login_manager = LoginManager(app)
 
 
 def connect_db() -> sqlite3.Connection:
@@ -16,8 +20,8 @@ def connect_db() -> sqlite3.Connection:
 
 def create_db() -> None:
     db = connect_db()
-    with app.open_resource("users.sql", mode="r") as f:
-        db.cursor().executescript(f.read())
+    with app.open_resource("users.sql", mode="r") as file:
+        db.cursor().executescript(file.read())
     db.commit()
     db.close()
 
@@ -36,34 +40,73 @@ def close_db(error: Exception | None) -> None:
 
 @app.route("/")
 def index() -> str:
-    return redirect(url_for("register"))
+    return redirect(url_for("login"))
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register() -> str:
     db = get_db()
     if request.method == "POST":
-        username, password, email = (
-            request.form.get("username"),
-            generate_password_hash(request.form.get("password")),
-            request.form.get("email"),
+        username, password = request.form.get("username"), generate_password_hash(
+            request.form.get("password")
         )
-        if username and password and email:
-            db.cursor().execute(
-                "INSERT INTO users(username,password,email) VALUES(?, ?, ?)",
-                (username, password, email),
-            )
-            flash("The user was added", category="success")
-            db.commit()
-            return redirect(url_for("users"))
+        if not username or not password:
+            flash("You didn't fill all the fills", category="error")
+        elif (
+            db.cursor()
+            .execute("SELECT username FROM users WHERE username = ?", (username,))
+            .fetchall()
+        ):
+            flash("The user exsists", category="error")
         else:
-            flash("Error", category="error")
+            db.cursor().execute(
+                "INSERT INTO users(username,password) VALUES(?,?)", (username, password)
+            )
+            db.commit()
+            return redirect(url_for("login"))
     return render_template("register.html")
 
 
-@app.route("/users")
-def users() -> str:
+@app.route("/login", methods=["GET", "POST"])
+def login() -> str:
     db = get_db()
-    return render_template(
-        "users.html", u=db.cursor().execute("SELECT * FROM users").fetchall()
-    )
+    if request.method == "POST":
+        user = (
+            db.cursor()
+            .execute(
+                "SELECT * FROM users WHERE username = ?",
+                (request.form.get("username"),),
+            )
+            .fetchall()
+        )
+        if user and check_password_hash(user[0][2], request.form.get("password")):
+            user_login = UserLogin().create(user)
+            login_user(user_login)
+            return redirect(url_for("profile"))
+        flash("Incorrect", category="error")
+    return render_template("login.html")
+
+
+@login_manager.user_loader
+def load_user(user_id: int) -> tuple:
+    db = get_db()
+    return UserLogin().fromDB(user_id, db)
+
+
+@app.route("/profile")
+def profile() -> str:
+    db = get_db()
+    if session:
+        return render_template(
+            "profile.html",
+            user=db.cursor().execute(
+                "SELECT username FROM users WHERE id = ?", (session.get("_user_id"),)
+            ).fetchone()[0],
+        )
+    return "Oops"
+
+
+@app.route("/logout")
+def logout() -> str:
+    session.clear()
+    return redirect(url_for("login"))
